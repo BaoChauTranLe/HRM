afc1f0210334e5ceb74e2a1db7bf749d4e51c981
Cúc Phan Thị Hồng <18520260@ms.uit.edu.vn>
2020-12-12 17:12:47 +0700

    Add login form

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,7 +12,10 @@ namespace HRM.Controllers
         {
             return View();
         }
-
+        public ActionResult Login()
+        {
+            return View();
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
