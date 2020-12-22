a914f13e8bc7671114f6c22c46228809efb1af42
Cúc Phan Thị Hồng <18520260@ms.uit.edu.vn>
2020-12-23 02:46:04 +0700

    Add TaxRate Create

--- Controllers/TAXRATEsController.cs
+++ Controllers/TAXRATEsController.cs
@@ -12,21 +12,17 @@ namespace HRM.Controllers
 {
     public class TAXRATEsController : Controller
     {
-        int? selectedRank;
-        public int? getselectedRank()
-        {
-            return selectedRank;
-        }
-        public void setselectedRank(int? rank)
-        {
-            selectedRank = rank;
-        }
         private hrmserver_HRMEntities db = new hrmserver_HRMEntities();
 
         // GET: TAXRATEs
         public ActionResult Index()
         {
             return View(db.TAXRATEs.ToList());
+        }
+
+        public ActionResult ToCreate()
+        {
+            return RedirectToAction("Create");
         }
 
         // GET: TAXRATEs/Create
@@ -40,12 +36,9 @@ namespace HRM.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Rank,Min,Max,Rate")] TAXRATE tAXRATE)
+        public ActionResult Create([Bind(Include = "Rank,Min,Rate")] TAXRATE tAXRATE)
         {
             tAXRATE.Rank = RankGenerator();
-            tAXRATE.Min = MinGenerator();
-            if (tAXRATE.Max != null && tAXRATE.Max <= tAXRATE.Min)
-                ModelState.AddModelError("Max", "Giá trị được nhập phải lớn hơn giá trị phía trước");
             if (ModelState.IsValid)
             {
                 db.TAXRATEs.Add(tAXRATE);
@@ -64,29 +57,10 @@ namespace HRM.Controllers
             if (taxRateList.Count > 0)
             {
                 taxRank = taxRateList.Last().Rank + 1;
-                //if (taxRateList.Last().Max != null)
-                    //taxRank = taxRateList.Last().Rank + 1;
-                //else
-                    //taxRank = taxRateList.Last().Rank;
             }
             return taxRank;
         }
 
-        public double? MinGenerator()
-        {
-            double? min = 0;
-            var taxRateList = db.TAXRATEs.ToList();
-            if (taxRateList.Count > 0)
-            {
-                min = taxRateList.Last().Max;
-                //double? max = taxRateList.Last().Max;
-                //if (max == null)
-                //min = taxRateList.Last().Min;
-                //else
-                //min = max;
-            }    
-            return min;
-        }
         public ActionResult BackToIndex()
         {
             return RedirectToAction("Index");
--- Models/TAXRATE.cs
+++ Models/TAXRATE.cs
@@ -11,14 +11,18 @@ namespace HRM.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class TAXRATE
     {
         public int Rank { get; set; }
+        
         [RegularExpression(@"[0-9]*\.?[0-9]*", ErrorMessage = "Giá trị được nhập phải là một số")]
+        [Required(ErrorMessage = "Phần thu nhập tính thuế / tháng không được để trống")]
         public int Min { get; set; }
-        [Required(ErrorMessage = "Thuế suất (%) không được để trống")]
+        
         [RegularExpression(@"[0-9]*\.?[0-9]*", ErrorMessage = "Giá trị được nhập phải là một số")]
+        [Required(ErrorMessage = "Thuế suất (%) không được để trống")]
         public int Rate { get; set; }
     }
 }
