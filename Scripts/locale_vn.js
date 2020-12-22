50bc767d488076247d4305cac858fd7e5a6eb2ab
Cúc Phan Thị Hồng <18520260@ms.uit.edu.vn>
2020-12-23 01:19:11 +0700

    Fix conflicts

---
+++ Scripts/locale_vn.js
@@ -0,0 +1,15 @@
+/*
+
+@license
+dhtmlxScheduler v.5.3.10 Professional Evaluation
+
+This software is covered by DHTMLX Evaluation License. Contact sales@dhtmlx.com to get Commercial or Enterprise license. Usage without proper license is prohibited.
+
+(c) XB Software Ltd.
+
+*/
+Scheduler.plugin(function(e){e.locale={date:{month_full:["Tháng 1, ","Tháng 2, ","Tháng 3, ","Tháng 4, ","Tháng 5, ","June","July","August","September","October","Tháng 11, ","Tháng 12, "],month_short:["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],day_full:["Chủ nhật","Thứ 2","Thứ 3","Thứ 4","Thứ 5","Thứ 6","Thứ 5 7"],day_short:["CN","T2","T3","T4","T5","T6","T7"]},labels:{dhx_cal_today_button:"Hôm nay",day_tab:"Ngày",week_tab:"Tuần",month_tab:"Tháng",new_event:"Ca làm việc",
+icon_save:"Save",icon_cancel:"Cancel",icon_details:"Details",icon_edit:"Edit",icon_delete:"Delete",confirm_closing:"",confirm_deleting:"Event will be deleted permanently, are you sure?",section_description:"Description",section_time:"Time period",full_day:"Full day",confirm_recurring:"Do you want to edit the whole set of repeated events?",section_recurring:"Repeat event",button_recurring:"Disabled",button_recurring_open:"Enabled",button_edit_series:"Edit series",
+button_edit_occurrence:"Edit occurrence",agenda_tab:"Agenda",date:"Date",description:"Description",year_tab:"Year",week_agenda_tab:"Agenda",grid_tab:"Grid",drag_to_create:"Drag to create",drag_to_move:"Drag to move",message_ok:"OK",message_cancel:"Cancel",next:"Next",prev:"Previous",year:"Year",month:"Month",day:"Day",hour:"Hour",minute:"Minute",repeat_radio_day:"Daily",repeat_radio_week:"Weekly",repeat_radio_month:"Monthly",repeat_radio_year:"Yearly",repeat_radio_day_type:"Every",
+repeat_text_day_count:"day",repeat_radio_day_type2:"Every workday",repeat_week:" Repeat every",repeat_text_week_count:"week next days:",repeat_radio_month_type:"Repeat",repeat_radio_month_start:"On",repeat_text_month_day:"day every",repeat_text_month_count:"month",repeat_text_month_count2_before:"every",repeat_text_month_count2_after:"month",repeat_year_label:"On",select_year_day2:"of",repeat_text_year_day:"day",select_year_month:"month",repeat_radio_end:"No end date",
+repeat_text_occurences_count:"occurrences",repeat_radio_end2:"After",repeat_radio_end3:"End by",month_for_recurring:["January","February","March","April","May","June","July","August","September","October","November","December"],day_for_recurring:["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]}}});
