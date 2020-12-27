4ce915bbef588fb820fcfad35810b8363b1e6567
Cúc Phan Thị Hồng <18520260@ms.uit.edu.vn>
2020-12-28 00:17:24 +0700

    Add Coefficients OverTime Salary view

--- Controllers/PARAMETERsController.cs
+++ Controllers/PARAMETERsController.cs
@@ -17,6 +17,9 @@ namespace HRM.Controllers
         {
             return View();
         }
-
+        public ActionResult CoefficientsOTSalary()
+        {
+            return View();
+        }
     }
 }
