0756e3349f6fdf5c7a6b3cf5456584744f32c886
Cúc Phan Thị Hồng <18520260@ms.uit.edu.vn>
2020-12-28 01:02:39 +0700

    Add User controller & Index view

---
+++ Controllers/USERsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HRM.Models;
+
+namespace HRM.Controllers
+{
+    public class USERsController : Controller
+    {
+        private hrmserver_HRMEntities db = new hrmserver_HRMEntities();
+
+        // GET: USERs
+        public ActionResult Index()
+        {
+            var uSERS = db.USERS.Include(u => u.EMPLOYEE);
+            return View(uSERS.ToList());
+        }
+
+        // GET: USERs/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USER uSER = db.USERS.Find(id);
+            if (uSER == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uSER);
+        }
+
+        // GET: USERs/Create
+        public ActionResult Create()
+        {
+            ViewBag.EmployeeID = new SelectList(db.EMPLOYEEs, "EmployeeID", "EmployeeName");
+            return View();
+        }
+
+        // POST: USERs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EmployeeID,Password")] USER uSER)
+        {
+            if (ModelState.IsValid)
+            {
+                db.USERS.Add(uSER);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.EmployeeID = new SelectList(db.EMPLOYEEs, "EmployeeID", "EmployeeName", uSER.EmployeeID);
+            return View(uSER);
+        }
+
+        // GET: USERs/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USER uSER = db.USERS.Find(id);
+            if (uSER == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.EmployeeID = new SelectList(db.EMPLOYEEs, "EmployeeID", "EmployeeName", uSER.EmployeeID);
+            return View(uSER);
+        }
+
+        // POST: USERs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EmployeeID,Password")] USER uSER)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(uSER).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.EmployeeID = new SelectList(db.EMPLOYEEs, "EmployeeID", "EmployeeName", uSER.EmployeeID);
+            return View(uSER);
+        }
+
+        // GET: USERs/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USER uSER = db.USERS.Find(id);
+            if (uSER == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uSER);
+        }
+
+        // POST: USERs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            USER uSER = db.USERS.Find(id);
+            db.USERS.Remove(uSER);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
