8300f9c63bedded5d7c81d2c9a8b50a21abcb3d6
HaPhuong1845 <52828501+HaPhuong1845@users.noreply.github.com>
2020-12-30 03:50:02 +0700

    Add import excel function

--- Controllers/TIMEKEEPINGREPORTsController.cs
+++ Controllers/TIMEKEEPINGREPORTsController.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Data.OleDb;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
+using LinqToExcel;
+using System.Data.SqlClient;
 using HRM.Models;
-
 namespace HRM.Controllers
 {
     public class TIMEKEEPINGREPORTsController : Controller
@@ -21,6 +26,102 @@ namespace HRM.Controllers
             return View(tIMEKEEPINGREPORTs.ToList());
         }
         // GET: TIMEKEEPINGREPORTs
+        [HttpPost]
+        public ActionResult UploadExcel(TIMEKEEPINGREPORT timekeepingsRP, HttpPostedFileBase FileUpload)
+        {
+            List<string> data = new List<string>();
+            if (FileUpload != null)
+            {
+                // tdata.ExecuteCommand("truncate table OtherCompanyAssets");  
+                if (FileUpload.ContentType == "application/vnd.ms-excel" || FileUpload.ContentType == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+                {
+                    string filename = FileUpload.FileName;
+                    string targetpath = Server.MapPath("~/Doc/");
+                    FileUpload.SaveAs(targetpath + filename);
+                    string pathToExcelFile = targetpath + filename;
+                    var connectionString = "";
+                    if (filename.EndsWith(".xls"))
+                    {
+                        connectionString = string.Format("Provider=Microsoft.Jet.OLEDB.4.0; data source={0}; Extended Properties=Excel 8.0;", pathToExcelFile);
+                    }
+                    else if (filename.EndsWith(".xlsx"))
+                    {
+                        connectionString = string.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=\"Excel 12.0 Xml;HDR=YES;IMEX=1\";", pathToExcelFile);
+                    }
+
+                    var adapter = new OleDbDataAdapter("SELECT * FROM [Sheet1$]", connectionString);
+                    var ds = new DataSet();
+
+                    adapter.Fill(ds, "ExcelTable");
+
+                    DataTable dtable = ds.Tables["ExcelTable"];
+
+                    string sheetName = "Sheet1";
+
+                    var excelFile = new ExcelQueryFactory(pathToExcelFile);
+                    var artistAlbums = from a in excelFile.Worksheet<TIMEKEEPINGREPORT>(sheetName) select a;
+
+                    foreach (var a in artistAlbums)
+                    {
+                        try
+                        {
+                            if (a.EmployeeID != null && a.Month != null)
+                            {
+                                TIMEKEEPINGREPORT TU = new TIMEKEEPINGREPORT();
+                                TU.Month = a.Month;
+                                TU.EmployeeID = a.EmployeeID;
+                                TU.SumWorkDay = a.SumWorkDay;
+                                TU.SumAbsentHaveSalary = a.SumAbsentHaveSalary;
+                                TU.SumAbsentNoSalary = a.SumAbsentNoSalary;
+                                TU.SumHourNightNormal = a.SumHourNormal;
+                                TU.SumHourDayOff = a.SumHourDayOff;
+                                TU.SumHourSpecialDayOff = a.SumHourSpecialDayOff;
+                                TU.SumHourNightSpecialDayOff = a.SumHourNightSpecialDayOff;
+                                TU.SumHourNightSpecialDayOffExtra = a.SumHourNightSpecialDayOff;
+                                TU.SumHourNightNormal = a.SumHourNightNormal;
+                                TU.SumHourNightNormalExtra = a.SumHourNightNormalExtra;
+                                TU.SumHourNightDayOff = a.SumHourNightDayOff;
+                                TU.SumHourNightDayOffExtra = a.SumHourNightDayOffExtra;
+                                db.TIMEKEEPINGREPORTs.Add(TU);
+                                db.SaveChanges();
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+
+                        catch (DbEntityValidationException ex)
+                        {
+                            foreach (var entityValidationErrors in ex.EntityValidationErrors)
+                            {
+                                foreach (var validationError in entityValidationErrors.ValidationErrors)
+                                {
+                                    Response.Write("Property: " + validationError.PropertyName + " Error: " + validationError.ErrorMessage);
+                                }
+
+                            }
+                        }
+                    }
+                    //deleting excel file from folder  
+                    if ((System.IO.File.Exists(pathToExcelFile)))
+                    {
+                        System.IO.File.Delete(pathToExcelFile);
+                    }
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                   
+                    return Content("<script language='javascript' type='text/javascript'>alert('Format file không đúng');</script>");
+                }
+            }
+            else
+            {
+                return Content("<script language='javascript' type='text/javascript'>alert('Chưa có file');</script>");
+            }
+
+        }
         public ActionResult OvertimeIndex()
         {
             var tIMEKEEPINGREPORTs = db.TIMEKEEPINGREPORTs.Include(t => t.EMPLOYEE);
