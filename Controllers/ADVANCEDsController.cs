2a11fd4fb4eba8136f164b3b86577ade165381b2
HaPhuong1845 <52828501+HaPhuong1845@users.noreply.github.com>
2021-01-05 18:00:23 +0700

    Fix Advance

--- Controllers/ADVANCEDsController.cs
+++ Controllers/ADVANCEDsController.cs
@@ -40,36 +40,79 @@ namespace HRM.Controllers
                 return Json(result, JsonRequestBehavior.AllowGet);
             }
         }
-
+        public ActionResult Create()
+        {
+            ViewBag.EmployeeID = new SelectList(db.EMPLOYEEs, "EmployeeID", "EmployeeName");
+            return View();
+        }
         // POST: ADVANCEDs/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult CreateOrEdit([Bind(Include = "EmployeeID,DateAdvanced,Value")] ADVANCED aDVANCED)
+        public ActionResult Create([Bind(Include = "EmployeeID,DateAdvanced,Value")] ADVANCED aDVANCED)
         {
             if (ModelState.IsValid)
+			{
+				DateTime minDate = new DateTime(1, 1, 1, 0, 0, 0);
+				if (aDVANCED.DateAdvanced == minDate)
+				{
+					DateTime now = DateTime.Now;
+					aDVANCED.DateAdvanced = new DateTime(now.Year, now.Month, now.Day);
+					db.ADVANCEDs.Add(aDVANCED);
+					db.SaveChanges();
+					return RedirectToAction("Index");
+				}
+				else
+				{
+					db.Entry(aDVANCED).State = EntityState.Modified;
+					db.SaveChanges();
+					return RedirectToAction("Index");
+				}
+			}
+			ViewBag.EmployeeID = new SelectList(db.EMPLOYEEs, "EmployeeID", "EmployeeName", aDVANCED.EmployeeID);
+			return View(aDVANCED);
+		}
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult CreateOrEdit([Bind(Include = "EmployeeID,DateAdvanced,Value")] ADVANCED aDVANCED)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        DateTime minDate = new DateTime(1, 1, 1, 0, 0, 0);
+        //        if(aDVANCED.DateAdvanced == minDate)
+        //        {
+        //            DateTime now = DateTime.Now;
+        //            aDVANCED.DateAdvanced = new DateTime(now.Year, now.Month, now.Day);
+        //            db.ADVANCEDs.Add(aDVANCED);
+        //            db.SaveChanges();
+        //            return RedirectToAction("Index");
+        //        }
+        //        else
+        //        {
+        //            db.Entry(aDVANCED).State = EntityState.Modified;
+        //            db.SaveChanges();
+        //            return RedirectToAction("Index");
+        //        }
+        //    }
+        //    ViewBag.EmployeeID = new SelectList(db.EMPLOYEEs, "EmployeeID", "EmployeeName", aDVANCED.EmployeeID);
+        //    return View(aDVANCED);
+        //}
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
             {
-                DateTime minDate = new DateTime(1, 1, 1, 0, 0, 0);
-                if(aDVANCED.DateAdvanced == minDate)
-                {
-                    DateTime now = DateTime.Now;
-                    aDVANCED.DateAdvanced = new DateTime(now.Year, now.Month, now.Day);
-                    db.ADVANCEDs.Add(aDVANCED);
-                    db.SaveChanges();
-                    return RedirectToAction("Index");
-                }
-                else
-                {
-                    db.Entry(aDVANCED).State = EntityState.Modified;
-                    db.SaveChanges();
-                    return RedirectToAction("Index");
-                }
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            ViewBag.EmployeeID = new SelectList(db.EMPLOYEEs, "EmployeeID", "EmployeeName", aDVANCED.EmployeeID);
+            ADVANCED aDVANCED = db.ADVANCEDs.Find(id);
+            if (aDVANCED == null)
+            {
+                return HttpNotFound();
+            }
+            //ViewBag.AllowanceID = new SelectList(db.ALLOWANCEs, "AllowanceID", "AllowanceName", aLLOWANCEDETAIL.AllowanceID);
+            //ViewBag.EmployeeID = new SelectList(db.EMPLOYEEs, "EmployeeID", "EmployeeName", aLLOWANCEDETAIL.EmployeeID);
             return View(aDVANCED);
         }
-
         [HttpPost, ActionName("Edit")]
         //[ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "EmployeeID,DateAdvanced,Value")] ADVANCED aDVANCED)
